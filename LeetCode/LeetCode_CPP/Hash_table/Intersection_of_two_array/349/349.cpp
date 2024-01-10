8cce089f7812d15c26800e9414979f370c54556c
Ning <719860478@qq.com>
2024-01-10 14:55:58 +0800

    补充unordered_set版本

--- LeetCode/LeetCode_CPP/Hash_table/Intersection_of_two_array/349/349.cpp
+++ LeetCode/LeetCode_CPP/Hash_table/Intersection_of_two_array/349/349.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution
 {
     public:
-        // 用哈希表
+        // 用哈希表 unordered_map
         vector<int> intersection1(vector<int>& nums1, vector<int>& nums2) 
         {
             if(0 == nums1.size() || 0 == nums2.size()){
@@ -29,8 +30,26 @@ class Solution
             return res;
         }
 
+        // 用哈希表 unordered_set (底层是红黑树)
+        vector<int> intersection2(vector<int>& nums1, vector<int>& nums2) 
+        {
+            if(0 == nums1.size() || 0 == nums2.size()){
+                return {};
+            }
+
+            unordered_set<int> res;                                         // 用set去重
+            unordered_set<int> temp(nums1.begin(), nums1.end());            // 直接用数组初始化哈希表
+
+            for(int num : nums2){
+                if(temp.find(num) != temp.end()){                           // 没找到的时候会返回数组末尾指针!
+                    res.insert(num);
+                }
+            }
+            return vector<int>(res.begin(), res.end());                     // 返回一个vector<int>
+        }
+
         // 用数组...
-        vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+        vector<int> intersection3(vector<int>& nums1, vector<int>& nums2) 
         {
             if(0 == nums1.size() || 0 == nums2.size()){
                 return {};
@@ -61,7 +80,7 @@ int main(void)
     nums1 = {4,9,5}, nums2 = {9,4,9,8,4};
     // nums1 = {}, nums2 = {9,4,9,8,4};
 
-    res = ans.intersection(nums1, nums2);
+    res = ans.intersection2(nums1, nums2);
     
     for(size_t i = 0; i < res.size(); i++){
         cout << res[i] << " ";
