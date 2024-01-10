730a3102c0772231c946a4bf8c9ff5b2bb7fc250
Ning <719860478@qq.com>
2024-01-10 11:49:00 +0800

    链表: LeetCode 49 字母异位词分组

--- LeetCode/LeetCode_CPP/Hash_table/Valid_anagram/49/49.cpp
+++ LeetCode/LeetCode_CPP/Hash_table/Valid_anagram/49/49.cpp
@@ -10,7 +10,7 @@ class Solution
             vector<vector<string> > res;
             unordered_map<string, int> hashMap;   // 用来判断排序后的单词是否存在, value值是int, 它用来区分不同的异位词字符串应该属于res的哪个部分
             string temp = "";
-            int sub = 0;                          // 用来区分不同的异位词
+            int res_index = 0;                    // 用来区分不同的异位词, 同时也是res中存放不同 "字母异位词" 的index
 
             for(int i = 0; i < strs.size(); ++i){
                 // 对每一个单词进行排序
@@ -18,13 +18,13 @@ class Solution
                 sort(temp.begin(), temp.end());
 
                 if(hashMap.count(temp) != 0){   // 当前单词已经出现过
-                    res[hashMap[temp]].push_back(strs[i]);      // 以 hashMap 的 value 作为索引, 将 strs[i] 放入res
+                    res[hashMap[temp]].push_back(strs[i]);  // 以 hashMap 的 value 作为索引, 将 strs[i] 放入res
                 }
-                else{   // 如果当前单词是第一次出现
-                    vector<string> vec(1, strs[i]);             
+                else{   // 如果当前单词是第一次出现   
+                    vector<string> vec(1, strs[i]);
                     res.push_back(vec); 
-                    hashMap[temp] = sub;
-                    sub++;                      // 用来区分不同的异位词, 每次使用完sub, sub都自增
+                    hashMap[temp] = res_index;
+                    res_index++;                      // 用来区分不同的异位词, 每次使用完sub, sub都自增
                 }
             }
             return res;
@@ -33,5 +33,22 @@ class Solution
 
 int main(void)
 {
+    vector<string> strs;
+    vector<vector<string>> res;
+
+    strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    Solution ans;
+    res = ans.groupAnagrams(strs);
+
+    cout << "[ ";
+    for(auto group:res){
+        cout << "[ ";
+        for(auto item:group){
+            cout << item << " ";
+        }
+        cout << "] ";
+    }
+    cout << "]" << endl;
+
     return 0;
 }
