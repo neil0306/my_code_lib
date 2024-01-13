4af70aac9019f8696b901ea78dd07e95be9a138c
Ning <719860478@qq.com>
2024-01-14 00:41:41 +0800

    字符串: LeetCode 459. 重复的子字符串

--- LeetCode/LeetCode_CPP/String/Repeat_subStr_pattern/459/459.cpp
+++ LeetCode/LeetCode_CPP/String/Repeat_subStr_pattern/459/459.cpp
@@ -18,7 +18,63 @@ class Solution
 
         bool repeatSubstringPattern_KMP(string s)
         {
+            int len = s.size();
+            int next[s.size()];
+            getNext(next, s);  // 遍历了一次原数组, 时间复杂度 O(m+n)
+
+            if(next[len-1] != 0 && len % (len - next[len-1]) == 0){
+                    return true;
+            }
+            return false;
+        }
+
+        // 前缀表直接作为Next数组
+        void getNext(int * next, string s)
+        {
+            // 初始化
+            int j = 0;
+            next[0] = 0;
             
+            for(int i = 1; i < s.size(); i++){
+                // 处理前后缀元素不相等
+                while(j > 0 && s[i] != s[j]){
+                    j = next[j-1];
+                }
+                // 处理前后缀元素相等
+                if(s[i] == s[j]){
+                    j++;
+                }
+                next[i] = j;
+            }
+        }
+
+        // 前缀表-1作为next数组
+        void getNext_sub1(int * next, string s)
+        {
+            int j = -1;
+            next[0] = j;
+
+            for(int i = 1; i < s.size(); ++i){
+                while(j >= 0 && s[i] != s[j+1]){
+                    j = next[j];
+                }
+                if(s[i] == s[j+1]){
+                    j++;
+                }
+                next[i] = j;
+            }
+        }
+
+        bool repeatSubstringPattern_KMP_sub1(string s)
+        {
+            int len = s.size();
+            int next[s.size()];
+            getNext_sub1(next, s);  // 遍历了一次原数组, 时间复杂度 O(m+n)
+
+            if(next[len-1] != -1 && len % (len - (next[len-1]+1)) == 0){
+                    return true;
+            }
+            return false;
         }
 };
 
@@ -27,7 +83,10 @@ int main(void)
     Solution ans;
     string s;
 
-    s = "aa";
+    s = "ababababababa";
+    s = "ababc";
     cout << (ans.repeatSubstringPattern(s)== true ? "true" : "false") << endl;
+    cout << (ans.repeatSubstringPattern_KMP(s)== true ? "true" : "false") << endl;
+    cout << (ans.repeatSubstringPattern_KMP_sub1(s)== true ? "true" : "false") << endl;
     return 0;
 }
